e30a8b1536cdbefe9e593fdc1048a31af63f41b0
11 <841850130@qq.com>
2018-08-23 11:56:30 +0800

    项目创建

---
+++ vue-eleven/src/vivw/helloEleven.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class='hello'>
+        <h2>hello VUE-ELEVEN</h2>
+        <p>什么是VUE-ELEVEN?</p>
+        <p>VUE-ELEVEN是一款基于Vue开发的插件库</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+.hello {
+    padding-top: 30px;
+    text-align: center; 
+}
+</style>
